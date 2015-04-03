00a5917e5f838c20e186a8eea6811b457cd159e4
Robert Nelson <robertn@the-nelsons.org>
2015-04-03 14:21:44 -0700

    Make cleanup script portable

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for name in *.parser; do
 	base=`basename $name .parser`
-	rm -rf $base{,.pl,.tar.gz}
+	rm -rf $base $base.pl $base.tar.gz
 done
-rm -rf framework{,.tar.gz}
+rm -rf framework framework.tar.gz
